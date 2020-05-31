534719cb2180b20f770fc469c0820fdda0166ac4
Casper Smet <casper.smet@student.hu.nl>
2020-05-31 18:15:02 +0200

    Added network and adapted pseudo code more

--- lib/simulation.py
+++ lib/simulation.py
@@ -1,4 +1,4 @@
-from classes.computer import Acceptor, Proposer, Network, Message
+from classes.computer import Acceptor, Computer, Proposer, Network, Message
 
 
 class Simulation:
@@ -17,17 +17,23 @@ class Simulation:
         self.tmax = tmax
         self.E = E
 
+        self.network = Network()
+
     def parse_events(self):
         """Returns generator object for Messages
 
         :yield: (ticks, Message, Failed computer, Recovered computer) describing event
         :rtype: (t, Message, F, R)
         """
+
+        current_t = self.E[0].split(" ")[0]
+        # Computers that fail this tick
+        F = []
+        # Computer that recover this tick
+        R = []
+        # Message that is sent during this tick
+        message = None
         for t, msg_type, target, value in map(lambda row: row.split(" "), self.E):
-            # Computers that fail this event
-            F = None
-            # Computer that recover this event
-            R = None
             # If the msg_type is PROPOSE, simply make a message
             if msg_type == "PROPOSE":
                 message = Message(
@@ -37,55 +43,84 @@ class Simulation:
             elif msg_type in ["FAIL", "RECOVER"]:
                 # If target is Proposer, grab the Proposer at index value-1
                 if target == "PROPOSER":
-                    F = Proposer.props[int(value) - 1]
+                    c = Proposer.props[int(value) - 1]
                 # If the target is Acceptor, grab the Acceptor at index value-1
                 elif target == "ACCEPTOR":
-                    R = Acceptor.acs[int(value) - 1]
+                    c = Acceptor.acs[int(value) - 1]
                 else:
                     print("Invalid target")
                     pass
+
+                if msg_type == "FAIL":
+                    F.append(c)
+                elif msg_type == "RECOVER":
+                    R.append(c)
             else:
                 print("Invalid message type")
                 pass
-            # Yield message for generator
-            yield int(t), message, F, R
+            if current_t != t:
+                # Yield message for generator
+                yield int(current_t), message, F, R
+                current_t = t
+                # Computers that fail this tick
+                F = []
+                # Computer that recover this tick
+                R = []
+                # Message that is sent during this tick
+                message = None
+        yield int(current_t), message, F, R
 
     def run(self):
         """[summary]
         """
         events = self.parse_events()
+        events_empty = False
+        
         tick, m, F, R = next(events)
-
         for t in range(self.tmax):
+            if events_empty and self.network.is_empty():
+                # If events_empty is true, and the queue is empty, exit simulation
+                # TODO add necessary prints here
+                return
+
             if t != tick:
-                print(f"{t}:")
-                pass
-            # print(tick, m, F, R)
-            print(f"{t}: {m}")
-            try:
-                event = next(events)
-            except StopIteration:
-                pass
-             
+                m = self.network.extract_message()
+                if m:
+                    print(f"{t:03}: {m}")
+                    Computer.deliver_message(m.dst, m)
+                else:
+                    # print(f"{t:03}:")
+                    pass
+            else:
+                # Fail all computers that failed during this tick
+                for c in F:
+                    c.failed = True
+                    print(f"{t:03}: ** {c} kapot **")
+                # Repair all computers that recovered during this tick
+                for c in R:
+                    c.failed = False
+                    print(f"{t:03}: ** {c} gerepareerd **")
+                
+                if m:
+                    print(f"{t:03}: {m}")
+                    Computer.deliver_message(m.dst, m)
+                else:
+                    m = self.network.extract_message()
+                    if m:
+                        print(f"{t:03}: {m}")
+                        Computer.deliver_message(m.dst, m)
+
+                try:
+                    tick, m, F, R = next(events)
+                except StopIteration:
+                    # If the end of events is reached, set events_empty to True
+                    events_empty = True
 
 
 if __name__ == "__main__":
-
     input_ = ["0 PROPOSE 1 42",
               "8 FAIL PROPOSER 1",
               "11 PROPOSE 2 37",
               "26 RECOVER PROPOSER 1"]
     sim = Simulation(2, 3, 50, input_)
     sim.run()
-    # Acceptor(1)
-    # Acceptor(2)
-    # Acceptor(3)
-    # # print(a.props, a.acs)
-    # Proposer(1)
-    # # print(p.props, p.acs)
-
-    # q = Network()
-    # # q.queue_message(Message(p, a, "PROPOSE", 0))
-    # # q.queue_message(Message(a, p, "ACCEPTED", 0))
-    # print(q.extract_message())
-    # print(q)
